7d6d1fafc4324e8d63d4f17f83f7f3ddfbb89e83
arihantsethia <arihantsethia07@gmail.com>
2013-03-24 07:59:29 +0530

    v1.0

--- runserver.py
+++ runserver.py
@@ -1,2 +1,13 @@
-from index import app
-app.run(debug=True)
+from __future__ import with_statement
+from sqlite3 import dbapi2 as sqlite3
+from flask import Flask
+from views import contest
+
+
+# create our little application :)
+app = Flask(__name__)
+app.register_blueprint(contest)
+
+if __name__ == '__main__':
+	app.run()
+
