3e944f29422d441824832ad90964b5557cb57f8f
arihantsethia <arihantsethia07@gmail.com>
2013-03-24 08:16:07 +0530

    v1.0

--- runserver.py
+++ runserver.py
@@ -1,12 +1,12 @@
 from __future__ import with_statement
 from sqlite3 import dbapi2 as sqlite3
 from flask import Flask
-from views import contest
+from views import views
 
 
 # create our little application :)
 app = Flask(__name__)
-app.register_blueprint(contest)
+app.register_blueprint(views)
 
 if __name__ == '__main__':
 	app.run()
