43429ff70dffc412a37a14290beb30bb4e662ae6
piyush <piyushdhore@gmail.com>
2013-03-25 10:26:00 +0530

    pd

--- contest_views.py
+++ contest_views.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template, redirect, url_for, Blueprint
-
+import contest_p
+import db from runserver
 contest_views = Blueprint('contest_views',__name__)
 
-@contest_views.route('/practice')
-def index():
-    return render_template("practice.html")
+@contest_views.route('/contests')
+def contest_list():
+    
+    #fetch current online contest from table contest
+    cur = db.execute('select time(end_time), from contest where time(current_timestamp, 'localtime') < time(end_time)')
+    contests_live=cur.fetchall();
+    #send live contest names to browser
+    return render_template("contests.html",contests=contests_live)
 
-@contest_views.route('/compete')
-def contest():
-    return render_template("compete.html")
+if __name__ == '__main__':
+    contest_views.run(Debug)
